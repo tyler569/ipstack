f6f407a07957cfd28965e7b00d52a4972bb14e91
Tyler Philbrick <t5j6p9@gmail.com>
2020-04-08 18:23:27 -0700

    tcp_echo: maybe I only need arpa/inet

--- tcp_echo.c
+++ tcp_echo.c
@@ -2,9 +2,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+/*
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+*/
 #include <arpa/inet.h>
 
 #define BUFLEN 2048
